5f78ddf38a17b4a679f475445591be22a3ce7c98
tamlt2704 <tamlt2704@gmail.com>
2018-04-07 17:28:14 +0800

    update code hanoi bus

--- hanoibus/process_data.py
+++ hanoibus/process_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import os
 
 
 def process_geo():
@@ -23,16 +24,30 @@ def process_bus_stop_code():
     with open(filename, 'w') as f:
         json.dump(js, f, ensure_ascii=False)
 
-# def process_bus_route():
+def process_bus_route():
+    bus_stop_code_df = pd.read_csv('static/BusStopCode.csv', index_col=[0])
 
-bus_stop_code_df = pd.read_csv('static/BusStopCode.csv', index_col=[0])
+    df = pd.read_csv('static/BusRoute.csv', index_col=[0])
+    for k, g in df.groupby(['Code', 'Direction']):
+        js = g.sort_values('StopSequence')[['BusStopCode']]
+        js = pd.merge(js, bus_stop_code_df, left_on='BusStopCode', right_on='BusStopCode', how='left')
+        js = js[['BusStopCode', 'FleetOver', 'Name', 'Lat', 'Lng']]
+        js = js.to_dict()
+        filename = 'static/route_{}_{}.json'.format(k[0], k[1])
+        with open(filename, 'w') as f:
+            json.dump(js, f)
 
-df = pd.read_csv('static/BusRoute.csv', index_col=[0])
-for k, g in df.groupby(['Code', 'Direction']):
-    js = g.sort_values('StopSequence')[['BusStopCode']]
-    js = pd.merge(js, bus_stop_code_df, left_on='BusStopCode', right_on='BusStopCode', how='left')
-    js = js[['BusStopCode', 'FleetOver', 'Name', 'Lat', 'Lng']]
-    js = js.to_dict()
-    filename = 'static/route_{}_{}.json'.format(k[0], k[1])
-    with open(filename, 'w') as f:
-        json.dump(js, f)
+
+# def process_bus_info():
+
+for fn in os.listdir('data/raw_data'):
+    if fn.startswith('fleed_'):
+        try:
+            with open('data/raw_data/{}'.format(fn), 'r') as f:
+                for line in f:
+                    js = json.loads(line)
+                    js = json.loads(js)
+
+                    print js['dt']['Data'][0]['Name']
+        except:
+            pass
