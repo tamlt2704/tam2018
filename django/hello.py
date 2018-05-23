bd71c30cdd0fa10ebec4cb7234c61dea75f65a5f
tamlt2704@gmail.com <tamlt2704@gmail.com>
2018-05-23 09:20:48 +0000

    3. additional settings

--- django/hello.py
+++ django/hello.py
@@ -3,8 +3,15 @@ import sys
 
 from django.conf import settings
 
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+
+print ALLOWED_HOSTS
 settings.configure(
-	DEBUG=True,
+	DEBUG=DEBUG,
+	SECRET_KEY=SECRET_KEY,
+	ALLOWED_HOSTS=ALLOWED_HOSTS,
 	ROOT_URLCONF=__name__,
 	MIDDLEWARE_CLASSES=(
 		'django.middleware.common.CommonMiddleware',
