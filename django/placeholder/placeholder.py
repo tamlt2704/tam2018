e07e8127aa01a1b4deb5e72e713d20592791a8a8
tamlt2704@gmail.com <tamlt2704@gmail.com>
2018-05-24 08:03:45 +0000

    6. static files

--- django/placeholder/placeholder.py
+++ django/placeholder/placeholder.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import hashlib
 from io import BytesIO
 from PIL import Image, ImageDraw
 
@@ -9,8 +10,8 @@ from django.conf import settings
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 SECRET_KEY = os.environ.get('SECRET_KEY', '984(x^oi0ra%&3vqy2fi2vxb73oftvbtl-vp&2t0)ex(qvq02v')
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+BASE_DIR = os.path.dirname(__file__)
 
-print ALLOWED_HOSTS
 settings.configure(
 	DEBUG=DEBUG,
 	SECRET_KEY=SECRET_KEY,
@@ -21,6 +22,19 @@ settings.configure(
 		'django.middleware.csrf.CsrfViewMiddleware',
 		'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	),
+	INSTALLED_APPS=(
+		'django.contrib.staticfiles',
+	),
+	TEMPLATES=(
+		{
+			 'BACKEND': 'django.template.backends.django.DjangoTemplates',
+			 'DIRS': (os.path.join(BASE_DIR, 'templates'), ),
+		},
+	),
+	STATICFILES_DIRS=(
+		os.path.join(BASE_DIR, 'static'),
+	),
+	STATIC_URL='/static/',
 )
 
 
@@ -28,6 +42,10 @@ from django import forms
 from django.conf.urls import url
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.core.wsgi import get_wsgi_application
+from django.core.cache import cache
+from django.views.decorators.http import etag
+from django.shortcuts import render
+from django.core.urlresolvers import reverse
 
 application = get_wsgi_application()
 
@@ -39,23 +57,35 @@ class ImageForm(forms.Form):
 		height = self.cleaned_data['height']
 		width = self.cleaned_data['width']
 
-		image = Image.new('RGB', (width, height))
+		
+		key = '{}.{}.{}'.format(width, height, image_format)
+		content = cache.get(key)
+		if content is None:	
+			image = Image.new('RGB', (width, height))
 
-		# draw text to image
-		draw = ImageDraw.Draw(image)
-		text = '{} x {}'.format(width, height)
-		textwidht, textheight = draw.textsize(text)
-		if textwidht < width and textheight < height:
-			texttop = (height - textheight) // 2
-			textleft = (width - textwidht) // 2
-			draw.text((textleft, texttop), text, fill=(255, 255, 255))
+			# draw text to image
+			draw = ImageDraw.Draw(image)
+			text = '{} x {}'.format(width, height)
+			textwidht, textheight = draw.textsize(text)
+			if textwidht < width and textheight < height:
+				texttop = (height - textheight) // 2
+				textleft = (width - textwidht) // 2
+				draw.text((textleft, texttop), text, fill=(255, 255, 255))
 			
-		content = BytesIO()
-		image.save(content, image_format)
-		content.seek(0)
-		return content
-	
+			content = BytesIO()
+			image.save(content, image_format)
+			content.seek(0)
 
+			cache.set(key, content, 60 * 60)
+		return content
+
+
+def generate_etag(request, width, height):
+	content = 'Placeholder {} x {}'.format(width, height)
+	return hashlib.sha1(content.encode('utf-8')).hexdigest()
+
+
+@etag(generate_etag)
 def placeholder(request, width, height):
 	form = ImageForm({'width': width, 'height': height})
 	if form.is_valid():
@@ -66,7 +96,11 @@ def placeholder(request, width, height):
 
 
 def index(request):
-	return HttpResponse('Hello World')
+	example = reverse('placeholder', kwargs={'width': 50, 'height': 50})
+	context = {
+		'example': request.build_absolute_uri(example)
+	}
+	return render(request, 'home.html', context=context)
 
 
 
