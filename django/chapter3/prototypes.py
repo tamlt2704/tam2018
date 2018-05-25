147ac341cba7ffbb0ceaceb7836c9db82e8c4d88
tamlt2704@gmail.com <tamlt2704@gmail.com>
2018-05-25 02:56:09 +0000

    6. static files

--- django/chapter3/prototypes.py
+++ django/chapter3/prototypes.py
@@ -8,7 +8,7 @@ from django.template import Template
 from django.utils._os import safe_join
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
-BASEDIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(__file__)
 
 settings.configure(
 	DEBUG=True,
@@ -20,11 +20,19 @@ settings.configure(
 		'django.contrib.staticfiles',
 		'sitebuilder',
 	),
+	TEMPLATES=(
+		{
+			 'BACKEND': 'django.template.backends.django.DjangoTemplates',
+			 'DIRS': [],
+			 'APP_DIRS': True,
+		},
+	),
 	STATIC_URL='/static/',
-	SITE_PAGES_DIRECTORY=os.path.join(BASEDIR, 'pages'),
+	SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
 )
 
 if __name__ == "__main__":
 	from django.core.management import execute_from_command_line
 	
 	execute_from_command_line(sys.argv)
+
--- django/chapter3/sitebuilder/urls.py
+++ django/chapter3/sitebuilder/urls.py
@@ -1,1 +1,8 @@
-urlpatterns=()
+from django.conf.urls import url 
+from .views import page
+
+
+urlpatterns=(
+	url(r'^$', page, name='homepage'),
+	url(r'^(?P<slug>[\w./-]+)$', page, name='page'),
+)
